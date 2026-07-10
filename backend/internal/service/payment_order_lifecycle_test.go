a733810fd47be4308cf179a3760ea07573540225
aix <aixlau@outlook.com>
2026-07-10 17:52:51 +0800

    test: restore payment lifecycle error import

--- backend/internal/service/payment_order_lifecycle_test.go
+++ backend/internal/service/payment_order_lifecycle_test.go
@@ -21,6 +21,7 @@ import (
 	"github.com/Wei-Shaw/sub2api/ent/enttest"
 	"github.com/Wei-Shaw/sub2api/ent/paymentauditlog"
 	"github.com/Wei-Shaw/sub2api/internal/payment"
+	infraerrors "github.com/Wei-Shaw/sub2api/internal/pkg/errors"
 	"github.com/Wei-Shaw/sub2api/internal/pkg/pagination"
 	"github.com/stretchr/testify/require"
 
